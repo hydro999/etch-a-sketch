fac7826e0297939b453a6ca818ddb75a90d066c6
hydro9 <67550848+hydro999@users.noreply.github.com>
2023-10-08 20:27:30 +0300

    update

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
 const container = document.querySelector(".container");
 
-// function createRow(input) {
-//   const rowWrap = document.createElement("div");
-//   rowWrap.setAttribute("class", "rowWrap");
-//   container.appendChild(rowWrap);
-//   for (let i = 0; i < input; i++) {
-//     const cell = document.createElement("div");
-//     cell.setAttribute("id", "cell");
-//     rowWrap.appendChild(cell);
-//   }
-// }
 function createRow(row) {
   const rowWrap = document.createElement("div");
   rowWrap.setAttribute("class", "rowWrap");
   container.appendChild(rowWrap);
   for (let i = 0; i < row; i++) {
     const cell = document.createElement("div");
-    cell.setAttribute("id", "cell");
+    cell.setAttribute("class", "cell");
+    //disables dragging that caused some unwanted bugs
+    cell.setAttribute("ondragstart", "return false");
     rowWrap.appendChild(cell);
   }
 }
@@ -29,3 +21,42 @@ function createColumn(column) {
 
 createRow(16);
 createColumn(16);
+
+let mouseDown = false;
+let mouseUp = true;
+
+document.body.addEventListener("mousedown", (event) => {
+  if (event.target.classList.contains("cell")) {
+    event.target.classList.add("black");
+    event.target.classList.remove("white");
+  }
+  mouseDown = !mouseDown; // toggle mouseDown true/false;
+});
+
+document.body.addEventListener("mousemove", function (event) {
+  if (event.target.classList.contains("cell")) {
+    if (mouseDown) {
+      event.target.classList.add("black");
+      event.target.classList.remove("white");
+    }
+  }
+});
+
+document.body.addEventListener("mouseup", (event) => {
+  mouseDown = !mouseDown; // toggle mouseDown true/false;
+});
+
+const getCells = document.querySelectorAll(".cell");
+
+function cell() {
+  for (let i = 0; i < getCells.length; i++) {
+    // getCells[i].style.backgroundColor = "white";
+    getCells[i].classList.add("white");
+    getCells[i].classList.remove("black");
+  }
+}
+
+const clear = document.querySelector("#clear");
+clear.addEventListener("click", function (event) {
+  cell();
+});
